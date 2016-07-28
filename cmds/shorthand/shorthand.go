92e56d7d95388ab17f03481f92dc91586a4b190f
R. S. Doiel <rsdoiel@gmail.com>
2016-07-28 12:51:40 -0700

    Assign Caltech copyright in source code

--- cmds/shorthand/shorthand.go
+++ cmds/shorthand/shorthand.go
@@ -3,10 +3,20 @@
 // and render output with the transformed text and without any
 // shorthand definitions.
 //
-// @author R. S. Doiel, <rsdoiel@gmail.com>
-// copyright (c) 2015 all rights reserved.
-// Released under the BSD 2-Clause license.
-// See: http://opensource.org/licenses/BSD-2-Clause
+// @author R. S. Doiel, <rsdoiel@caltech.edu>
+//
+// Copyright (c) 2016, Caltech
+// All rights not granted herein are expressly reserved by Caltech.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 package main
 
@@ -25,13 +35,13 @@ import (
 type expressionList []string
 
 var (
-	help        bool
-	version     bool
-	expression  expressionList
-	prompt      string
-	noprompt    bool
-	vm          *shorthand.VirtualMachine
-	lineNo      int
+	help                    bool
+	version                 bool
+	expression              expressionList
+	prompt                  string
+	noprompt                bool
+	vm                      *shorthand.VirtualMachine
+	lineNo                  int
 	postProcessWithMarkdown bool
 )
 
